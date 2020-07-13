17e0bae28763df89983a5a8cb0a12b5a4f5f3e64
Dane Matrix <quixotedane@gmail.com>
2020-07-13 18:45:32 -0400

    added styling menu-item

--- src/components/menu-item/menu-item-component.jsx
+++ src/components/menu-item/menu-item-component.jsx
@@ -3,15 +3,18 @@ import "./menu-item/styles.scss";
 //initializing MenuItem, using destructuring instead of props
 const MenuItem = ({ title, imageUrl, size }) => {
   <div
-    style={{
-      //string interpolation from the destructured props that loads the image as the background instead of using an img tag
-      backgroundImage: `url(${imageUrl})`,
-    }}
     //adding the 'large' class dynamically if it is found when pulled from destructuring.
     className={`${size} menu-item`}
   >
+    <div
+      className="background-image"
+      style={{
+        //string interpolation from the destructured props that loads the image as the background instead of using an img tag
+        backgroundImage: `url(${imageUrl})`,
+      }}
+    ></div>
     <div className="content">
-      <h1 className="title">{title}</h1>
+      <h1 className="title">{title.toUpperCase()}</h1>
       <span className="sustitle">Shop Now</span>
     </div>
   </div>;
